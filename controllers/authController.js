54154fdbbd18e46b127b19821e8c2a4814ea39e0
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-07-17 14:27:27 +0300

    hashing passwords

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,13 @@
 const router = require('express').Router()
-const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
+
 
 const authService = require('../service/authService')
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 router.get('/login', (req, res) => {
     res.render('login'); // 'register' трябва да съответства на името на вашия .hbs файл без разширение
 });
-
-
 
 
 //register html page
@@ -17,10 +16,37 @@ router.get('/register', (req, res) => {
 });
 
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
+    console.log(req.body);
+    try {
+        const { username, email, password } = req.body
 
-    const userData = req.body;
-    console.log(userData);
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send('User already exists')
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        // Save the user to the database
+        await newUser.save()
+            // .then(res => console.log(res))
+        // Redirect to the homepage
+        res.redirect('/'); // Adjust the path as necessary
+    }
+    catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).send('Internal server error');
+    }
+
+    // const userData = req.body;
+    // console.log(userData);
 })
 
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes')
 // const registerRoute = require('./routes');
 // const loginRoute = require('./routes');
-
+const authRouter = require('./controllers/authController')
 
 const port = 3000;
 const app = express();
@@ -16,7 +16,7 @@ require('./config/express')(app)
 app.use(routes)
 // app.use('/register', registerRoute);
 // app.use('/login', loginRoute);
-
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
     console.log(`server is running at ${port}`);
